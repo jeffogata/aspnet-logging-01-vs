8b9ce5944e1c30c2998ad4eee0815087bf01aafb
Jeff Ogata <jeffogata@outlook.com>
2016-01-29 21:11:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# aspnet-logging-01-vs
+
+An example illustrating logging in ASP.NET 5, accompanying the [ASP.NET 5 Logging](https://jeffogata.com/asp-net-5-logging/) blog post.
+
+Tested with:
+
+* ASP.NET 5 RC1 Update 1
+* Windows 10, Visual Studio 2015 Update 1
---
+++ src/aspnet-logging-01-vs/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace aspnet_logging_01_vs
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<MyClass>();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Debug;
+
+            loggerFactory.AddDebug(LogLevel.Debug);
+                
+            app.UseIISPlatformHandler();
+
+            app.Run(async context =>
+            {
+                var myClass = context.RequestServices.GetService<MyClass>();
+
+                myClass.DoSomething(1);
+                myClass.DoSomething(20);
+                myClass.DoSomething(-20);
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
